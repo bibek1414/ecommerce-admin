1e44b869545c3f50479224a6088ab1d56b070982
bibek1414 <bbhattarai770@gmail.com>
2023-10-19 16:00:19 +0545

    final

---
+++ pages/settings.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+export default function SettingPage(){
+    return(
+        <Layout>
+            <h1>
+                Hello
+            </h1>
+        </Layout>
+    )
+}
